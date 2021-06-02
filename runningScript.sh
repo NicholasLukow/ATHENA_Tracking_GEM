f045562aaae599b1023bf9ab202cb150cc934e31
Nicholas S Lukow <lukown@eic0101.rcf.bnl.gov>
2021-06-02 15:21:37 -0400

    Minor modifications

--- runningScript.sh
+++ runningScript.sh
@@ -1,18 +1,37 @@
 #/bin/bash
-nEvents=5000
+nEvents=1000
 count=0
-#for pmin in 0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 9.5  ; do 
-#root -b -l -q './Fun4All_G4_HybridGEM.C('$nEvents' , '$pmin', '$pmin'+1,'$etamin', '$etamin'+0.25, 4, "job'$count'")'
-#let count=count+1
-#done
-#for pmin in 11 13 15 17 19  ; do 
-#root -b -l -q './Fun4All_G4_HybridGEM.C('$nEvents' , '$pmin', '$pmin'+2,'$etamin', '$etamin'+0.25, 4, "job'$count'")'
-#let count=count+1
-#done
 
+etamin=1.0
+etamax=3.5
+etainc=0.5
+eta=$etamin
+
+
+while [ "$(bc <<< "$eta < $etamax")" == "1" ]; do
 pmin=1
 pmax=20
-for etamin in 0 0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.25 2.5 2.75 3.0 3.25 ; do 
-root -b -l -q './Fun4All_G4_HybridGEM.C('$nEvents' , '$pmin', '$pmax','$etamin', '$etamin'+0.25, 4, "job'$count'_NoGEM")'
-let count=count+1
+pinc=1
+p=$pmin
+
+	while [ "$(bc <<< "$p < $pmax")" == "1" ] ; do
+		ptemp=$(bc <<< "$p+$pinc")
+		etatemp=$(bc <<< "$eta+$etainc")
+		
+		#Output Root file name identifies the parameters of the generator
+		root -b -l -q './Fun4All_G4_HybridGEM.C('$nEvents' , '$p', '$ptemp','$eta', '$etatemp', 4, "P_'$p'_'$ptemp'_Eta_'$eta'_'$etatemp'_")'
+		echo Eta: $eta P: $p 
+
+
+		let count=count+1
+		p=$(bc <<< "$p+$pinc")
+		
+		#incrementing by 2 above 10
+		if [ "$(bc <<< "$p == 10.0")" == "1" ]; then
+			pinc=2
+		fi
+
+	done
+	eta=$(bc <<< "$eta+$etainc")
+
 done
