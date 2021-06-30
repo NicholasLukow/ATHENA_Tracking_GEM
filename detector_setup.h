3667395374bf9372b12c713febc753f13defb100
Nicholas S Lukow <lukown@eic0111.rcf.bnl.gov>
2021-06-30 15:03:36 -0400

    removed option for beampipe extensions

--- detector_setup.h
+++ detector_setup.h
@@ -1,37 +1,35 @@
 //Beam pipe                                                                                                                                           
 
 #define _BEAMPIPE_ //comment out this line for no beam pipe                                                                                           
-//#define _USE_FORWARD_PIPES_ //To use the beampipe extensions. Slows down simulations                                                                  
 
 //Silicon Barrel                                                                                                                                      
 //For current version, both the vertexing layers and the barrel layers are attached to this
 #define _SIBARR_ //comment out this line for no silicon barrel                                                                                        
 
 //Silicon Vertex
+#define _SIVTX_
+
 //#define _EICTOYVST_
+//Simplified Version
+//#define _SIMPLEVST_
 
+//Silicon Disks                                                                                                                                       
+#define _SIDISKS_
+//#define _ALSUPP_
 
+//RICH
+#define _RICH_
 
-
+//DIRC
+//#define _DIRC_
 
 
 // MPGD                                                                                                                                               
 #define _MPGD_
 
 
-//Silicon Disks                                                                                                                                       
-#define _SIDISKS_
-//#define _ALSUPP_
-
 //GEMS                                                                                                                                                
-
 #define _GEMS_
 
-//Black holes to catch all hits outside of colume                                                                                                     
-//#define _BLACKHOLES_                                                                                                                                
-
-
-
-
-
-
+//Black holes to catch all hits outside of volume                                                                                                     
+#define _BLACKHOLE_                                                                                                                                
